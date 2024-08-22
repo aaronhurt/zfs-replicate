da42661d4307a6baff58b86edaeb9463b8a6389f
tschettervictor <85497460+tschettervictor@users.noreply.github.com>
2024-08-22 08:13:10 -0600

    Update status-report.sh
    changed log directory to /var/log/zfs-replicate

--- status-report.sh
+++ status-report.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-# Set default script directory
-SCRIPT=$(readlink -f "$0")
-SCRIPTPATH=$(dirname "${SCRIPT}")
+# Set log directory
+LOGS="/var/log/zfs-replicate"
 
 # Check for existing logs
-if ! [ -e  ${SCRIPTPATH}/logs ]; then
+if ! [ -e  ${LOGS} ]; then
 	echo "Log directory does not exist, can't check status."
 	exit 0
 fi
-
-# Set log directory
-LOGS="${SCRIPTPATH}/logs"
 
 # Retrieve latest log status
 RECENT_LOG_FILE=$(ls ${LOGS} | grep autorep- | tail -n 1)
