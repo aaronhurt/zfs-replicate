6e49f8d39a9c27e0ec7f5c038288807b6fe6bd40
tschettervictor <85497460+tschettervictor@users.noreply.github.com>
2024-08-22 18:52:53 -0600

    Update get-last-status.sh
    added section for XigmaNAS users to allow changing of LOGBASE variable if required

--- get-last-status.sh
+++ get-last-status.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-# Set log directory
-LOG_DIRECTORY="/var/log/zfs-replicate"
+# Set log directory (defaults to /var/log/zfs-replicate)
+## for XigmaNAS users, uncomment the next 3 lines
+## called SCRIPT SCRIPTPATH and LOGBASE
+## and comment out the last line to make sure the 
+## log files stay in the script directory
+#SCRIPT=$(readlink -f "$0")
+#SCRIPTPATH=$(dirname "${SCRIPT}")
+#LOGBASE="${SCRIPTPATH}/logs"
+LOGBASE="/var/log/zfs-replicate"
 
 # Check for existing logs
-if ! [ -e  ${LOG_DIRECTORY} ]; then
+if ! [ -e  ${LOGBASE} ]; then
 	echo "Log directory does not exist, can't check status."
 	exit 0
 fi
 
 # Retrieve latest log status
-RECENT_LOG_FILE=$(ls ${LOG_DIRECTORY} | grep autorep- | tail -n 1)
-STATUS=$(tail -n 1 ${LOG_DIRECTORY}/${RECENT_LOG_FILE})
+RECENT_LOG_FILE=$(ls ${LOGBASE} | grep autorep- | tail -n 1)
+STATUS=$(tail -n 1 ${LOGBASE}/${RECENT_LOG_FILE})
 
 echo "Last Replication Status"
 echo "----------"
