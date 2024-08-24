7fba2256fe341831089b88a0d72a3c82d52c8f04
tschettervictor <85497460+tschettervictor@users.noreply.github.com>
2024-08-24 12:42:29 -0600

    double quotes added

--- get-last-status.sh
+++ get-last-status.sh
@@ -11,14 +11,14 @@
 LOGBASE="/var/log/zfs-replicate"
 
 # Check for existing logs
-if ! [ -e  ${LOGBASE} ]; then
+if ! [ -e  "${LOGBASE}" ]; then
 	echo "Log directory does not exist, can't check status."
 	exit 0
 fi
 
 # Retrieve latest log status
-RECENT_LOG_FILE=$(ls ${LOGBASE} | grep autorep- | tail -n 1)
-STATUS=$(tail -n 1 ${LOGBASE}/${RECENT_LOG_FILE})
+RECENT_LOG_FILE=$(ls "${LOGBASE}" | grep autorep- | tail -n 1)
+STATUS=$(tail -n 1 "${LOGBASE}"/"${RECENT_LOG_FILE}")
 
 echo "Last Replication Status"
 echo "----------"
