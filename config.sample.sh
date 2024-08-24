d412405ebe40a998334c8936d8c503ff0e1d7816
Aaron Hurt <ahurt@anbcs.com>
2024-08-24 12:16:24 -0500

    cleanup get-last-status.sh

--- config.sample.sh
+++ config.sample.sh
@@ -1,97 +1,111 @@
-#!/bin/bash
-## zfs-replicate configuration file - edit as needed
-## config.sh
+#!/usr/bin/env bash
+## zfs-replicate configuration file
+# shellcheck disable=SC2034
 
-## ip address or hostname of a remote server
-## not used if TYPE is set to LOCAL
-REMOTE_SERVER="192.168.1.250"
-
-## set replication mode, PUSH or PULL
-## PULL replicates from remote to local
-## PUSH replicates from local to remote
-## default is PULL
-MODE="PULL"
-
-## set replication TYPE to LOCAL for local only replication
-## or REMOTE for remote replication
-## REMOTE - remote server replication (default)
-## LOCAL - local dataset replication
-TYPE="REMOTE"
-
-## datasets to replicate - use zfs paths not mount points...
-## format is localpool/localdataset:remotepool or
-## localpool/localdataset:remotepool/remotedataset
-## can include multiple strings separated by a "space"
-## pools and dataset pairs must exist on the respective servers
-## PUSH will push the local to the remote
-## PULL will pull the local to the remote
+## Datasets to replicate. These must be zfs paths not mount points.
+## The format general format is "source:destination".
+## Examples replicating a local source to a remote destination (PUSH):
+##   - sourcePool/sourceDataset:destinationPool@host
+##   - sourcePool/sourceDataset:destinationPool/destinationDataset@host
+## Examples replicating from a remote source to a local destination (PULL):
+##   - sourcePool/sourceDataset@host:destinationPool
+##   - sourcePool/sourceDataset@host:destinationPool/destinationDataset
+## Examples replicating a local source to a local destination:
+##   - sourcePool/sourceDataset:destinationPool
+##   - sourcePool/sourceDataset:destinationPool/destinationDataset
+## Multiple space separated sets may be specified.
+## Pools and dataset pairs must exist on the respective servers.
 REPLICATE_SETS="localpool/localdataset:remotepool/remotedataset"
 
-## option to recursively snapshot children of all datasets listed above
+## Command to check the health of remote host a return code of 0 is
+## considered OK/available. This is only be used when a replicate set
+## contains an "@host" designation. The macro string "%HOST%" will be
+## substituted with the value of the "@host" target in the replicate set.
+## The default command is "ping -c1 -q -W2 %HOST%".
+#REMOTE_CHECK="ping -c1 -q -W2 %HOST%"
+
+## Option to recursively snapshot children of all datasets listed above.
 ## 0 - disable (default)
 ## 1 - enable
-RECURSE_CHILDREN=0
+#RECURSE_CHILDREN=0
 
-## if ANY replication task results in an error because of either
-##  - no common snapshot
-##  - snapshots detected on destination
-##  - failure to replicate incremental snapshot
-## this will start the replication from scratch if set to 1
-## and overwrite the existing data on the destination
+## Option to force ANY replication task that would fail for any of the
+## following conditions:
+##   - no common snapshot
+##   - snapshots detected on destination
+##   - failure to replicate incremental snapshot
+## Setting "FORCE_REPLICATE" to 1 will start the replication from scratch
+## and overwrite the existing data on the destination.
 ## 0 - disable (default)
 ## 1 - enable (use at your own risk)
-ALLOW_REPLICATE_FROM_SCRATCH=0
+#FORCE_REPLICATE=0
 
-## Allow replication of root datasets
-## if you specify root datasets above and do not toggle this setting the
-## script will generate a warning and skip replicating root datasets
+## Allow replication of root datasets.
+## If "REPLICATE_SETS" contains root datasets and "ALLOW_ROOT_DATASETS" is
+## NOT seet to 1, root datasets will be skipped and a warning will be printed.
 ## 0 - disable (default)
 ## 1 - enable (use at your own risk)
-ALLOW_ROOT_DATASETS=0
+#ALLOW_ROOT_DATASETS=0
 
-## number of snapshots to keep for each dataset
-## older snapshots will be deleted
-SNAP_KEEP=2
+## The number of snapshots to keep for each dataset.
+## Older snapshots, by creation date, will be deleted.
+## A minimum of 2 snapshots must be kept for replication to work.
+## This defaults to 2 if not set.
+#SNAP_KEEP=2
 
-## number of logs to keep
-## older logs will be deleted
-LOG_KEEP=5
+## The following substitutions for current date information
+## may be used in the "TAG" setting below.
+## These are evaluated at runtime.
+##   - %DOW% = Day of Week (date "+%a")
+##   - %MOY% = Month of Year (date "+%m")
+##   - %DOM% = Day of Month (date "+%d")
+##   - %CYR% = Current Year (date "+%Y")
+##   - %NOW% = Current Unixtime (date "+%s")
 
-## log files directory (defaults to script path)
-## for XigmaNAS users, uncomment the next 3 lines
-## called SCRIPT SCRIPTPATH and LOGBASE
-## and comment out the last line to make sure the 
-## log files stay in the script directory
-#SCRIPT=$(readlink -f "$0")
-#SCRIPTPATH=$(dirname "${SCRIPT}")
-#LOGBASE="${SCRIPTPATH}/logs"
-LOGBASE="/var/log/zfs-replicate"
+## String used for snapshot names and log tags.
+## Example: pool0/someplace@autorep-08242024_1724527527
+## The default is "%MOY%%DOM%%CYR%_%NOW%"
+#TAG="%MOY%%DOM%%CYR%_%NOW%"
 
-## command to check health of remote host
-## a return code of 0 will be considered OK
-## not used if TYPE is set to LOCAL
-REMOTE_CHECK="ping -c1 -q -W2 ${REMOTE_SERVER}"
+## Option to write logs to syslog via the "logger" tool.
+## 0 - disable
+## 1 - enable (default)
+#SYSLOG=1
 
-## path to zfs binary (only command for now)
-ZFS=zfs
+## Optional logging facility to use with syslog. The default facility is
+## "user" unless changed below. Other common options local facic facilities 0-7.
+## Example: local0, local1, local2, local3, local4, local5, local6, or local7
+#SYSLOG_FACILITY="user"
 
-## path to GNU find binary
-## solaris `find` does not support the -maxdepth option, which is required
-## on solaris 11, GNU find is typically located at /usr/bin/gfind
-FIND=/usr/bin/find
+## Set the destination for physical log files to reside. By default
+## logging is done via syslog. This setting will always be treated as a
+## directory and not a file.
+#LOG_BASE="/var/log/zfs-replicate"
 
-## get the current date info
-DOW=$(date "+%a")
-MOY=$(date "+%m")
-DOM=$(date "+%d")
-NOW=$(date "+%s")
-CYR=$(date "+%Y")
+## The log file needs to start with "autorep-" in order for log cleanup
+## to work using the default below is strongly suggested. Leaving this commented out
+## will disable the writing of the standalone log file. The "%TAG%" substitution
+## and/or other date substitutions may be used. The default is "autorep-%TAG%.log"
+## When enabled logs will be placed under the "LOG_BASE" path set above.
+#LOG_FILE="autorep-%TAG%.log"
 
-## snapshot and log name tags
-## ie: pool0/someplace@autorep-${NAMETAG}
-NAMETAG="${MOY}${DOM}${CYR}_${NOW}"
+## Number of log files to keep. Note, this is only used
+## if "LOG_BASE" is set to a non-empty value above.
+## Older logs, by creation date, will be deleted.
+## This defaults to 5 if not set.
+#LOG_KEEP=5
 
-## the log file needs to start with
-## autorep- in order for log cleanup to work
-## using the default below is strongly suggested
-LOGFILE="${LOGBASE}/autorep-${NAMETAG}.log"
+## Path to the system "logger" executable.
+## The default uses the first "logger" executable found in $PATH.
+#LOGGER=$(which logger)
+
+## Path to GNU "find" binary. Solaris find does not support the "-maxdepth"
+## option, which is required to rotate log files.
+## On solaris 11, GNU find is typically located at "/usr/bin/gfind".
+## The default uses the first "find" executable in $PATH.
+## This is NOT required when using syslog.
+#FIND=$(which find)
+
+## Path to the system "zfs" binary. The default uses the first "zfs"
+## executable found in $PATH.
+#ZFS=$(which zfs)
--- get-last-status.sh
+++ get-last-status.sh
@@ -1,26 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
+## get-last-status.sh
 
-# Set log directory (defaults to /var/log/zfs-replicate)
-## for XigmaNAS users, uncomment the next 3 lines
-## called SCRIPT SCRIPTPATH and LOGBASE
-## and comment out the last line to make sure the 
-## log files stay in the script directory
-#SCRIPT=$(readlink -f "$0")
-#SCRIPTPATH=$(dirname "${SCRIPT}")
-#LOGBASE="${SCRIPTPATH}/logs"
-LOGBASE="/var/log/zfs-replicate"
+print_status() {
+  ## Set local variables used below
+  local logPath find
+  logPath="$(dirname "$(readlink -f "$0")")/logs"
+  find=$(which find)
 
-# Check for existing logs
-if ! [ -e  "${LOGBASE}" ]; then
-	echo "Log directory does not exist, can't check status."
-	exit 0
-fi
+  ## Check for existing logs
+  if ! [ -e "${logPath}" ]; then
+    printf "Log directory does not exist, can't check status.\n"
+    exit 0
+  fi
 
-# Retrieve latest log status
-RECENT_LOG_FILE=$(ls "${LOGBASE}" | grep autorep- | tail -n 1)
-STATUS=$(tail -n 1 "${LOGBASE}"/"${RECENT_LOG_FILE}")
+  ## Retrieve latest log status
+  local newestLog status
+  newestLog=$(${find} "${logPath}" -maxdepth 1 -type f -name autorep-\* | sort | tail -1)
+  status=$(tail -n 1 "${logPath}/${newestLog}")
 
-echo "Last Replication Status"
-echo "----------"
-echo "${STATUS}"
-echo "----------"
+  ## Print status block
+  printf "Last Replication Status\n----------\n%s\n----------\n" "${status}"
+}
